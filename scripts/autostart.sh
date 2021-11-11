dd82ed16f0bb383b1417d799f9d5b8c22ea17ee7
XXX <32349731+fakeGenuis@users.noreply.github.com>
2021-11-12 03:58:25 +0800

    [change] to i3lock and xidlehook

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -7,7 +7,6 @@ function run {
   fi
 }
 
-run light-locker
 run /usr/lib/polkit-kde-authentication-agent-1
 run picom --experimental-backends
 run redshift-gtk
@@ -16,3 +15,30 @@ run blueman-applet
 run nextcloud
 run qv2ray
 fcitx5 -rd
+
+# set screen saver time
+xset s 910
+
+# Run xidlehook
+if ! pgrep -f xidlehook ; then
+  xidlehook \
+    --detect-sleep \
+    `# Don't lock when there's a fullscreen application` \
+    --not-when-fullscreen \
+    `# Don't lock when there's audio playing` \
+    --not-when-audio \
+    `# Dim the screen after 60 seconds, undim if user becomes active` \
+    --timer 900 \
+      `# xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
+      `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1` \
+      'pgrep i3lock > /dev/null || notify-send -u critical "xidlehook" "Screen is about to lock" -i Fish -a ""' \
+      '' \
+    `# Undim & lock after 10 more seconds` \
+    --timer 10 \
+      'pgrep i3lock > /dev/null || myi3lock' \
+      '' \
+    `# Finally, suspend an hour after it locks`
+  #   --timer 3600 \
+  #     'systemctl suspend' \
+  #     ''
+fi
---
+++ scripts/i3lock.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# to use it in awesome and systemd
+# symbol link with 'sudo ln -s (realpath i3lock.sh) /usr/bin/myi3lock'
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#ff00ffcc'
+TEXT='#ee00eeee'
+WRONG='#880000bb'
+VERIFYING='#bb00bbbb'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 7                     \
+--clock                      \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %m %Y"       \
+--keylayout 1                \
+--indicator                  \
