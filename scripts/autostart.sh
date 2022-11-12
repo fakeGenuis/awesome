38f41cf0e7c543cb9ef1b3c63e70f0f3275e826e
JiaRan <32349731+fakeGenuis@users.noreply.github.com>
2022-11-12 15:42:41 +0800

    [update] wallpaper changer and autostart script

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,42 +6,44 @@ function run {
   fi
 }
 
-run xsettingsd -c ~/.config/xsettingsd/xsettingsd.conf
+run xsettingsd -c ~/.cache/xsettingsd/xsettingsd.conf
 run /usr/bin/lxqt-policykit-agent
 run picom --experimental-backends
 run redshift-gtk
 run kdeconnect-indicator
 run blueman-applet
 run qv2ray
-run xcape -e "Control_L=Escape;Super_R=Return;Alt_R=BackSpace"
+# if one use =via= ...
+run xcape -e "Control_L=Escape"
 
 fcitx5 -rd
 pidof nextcloud || nextcloud &
+
+# systemd (=~/.config/systemd/user/emacs.service=) emacs cannot inherit
+# environment variables in shell startup
 emacsclient -e "1" &>/dev/null || env LC_CTYPE='zh_CN.UTF-8' emacs --daemon &
 
 # set screen saver time
 xset s 910
 
 # Run xidlehook
-if ! pgrep -f xidlehook; then
-  xidlehook \
-    --detect-sleep \
-    `# Don't lock when there's a fullscreen application` \
-    --not-when-fullscreen \
-    `# Don't lock when there's audio playing` \
-    `# --not-when-audio` \
-    `# Dim the screen after 60 seconds, undim if user becomes active` \
-    --timer 900 \
-    `# xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
-    `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1` \
-    'notify-send -u critical "xidlehook" "Screen is about to lock" -i xidlehook -a ""' \
-    '' \
-    `# Undim & lock after 10 more seconds` \
-    --timer 10 \
-    'betterlockscreen -l' \
-    '' \
-    `# Finally, suspend an hour after it locks`
-  #   --timer 3600 \
-  #     'systemctl suspend' \
-  #     ''
-fi
+run xidlehook \
+  --detect-sleep \
+  `# Don't lock when there's a fullscreen application` \
+  --not-when-fullscreen \
+  `# Don't lock when there's audio playing` \
+  `# --not-when-audio` \
+  `# Dim the screen after 60 seconds, undim if user becomes active` \
+  --timer 900 \
+  `# xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
+  `# xrandr --output "$PRIMARY_DISPLAY" --brightness 1` \
+  'notify-send -u critical "xidlehook" "Screen is about to lock" -i xidlehook -a ""' \
+  '' \
+  `# Undim & lock after 10 more seconds` \
+  --timer 10 \
+  'betterlockscreen -l' \
+  '' \
+  `# Finally, suspend an hour after it locks`
+#   --timer 3600 \
+#     'systemctl suspend' \
+#     ''
--- scripts/wallpaper.fish
+++ scripts/wallpaper.fish
@@ -2,5 +2,50 @@
 
 # `crontab -e`
 # `*/5 * * * * ~/.config/awesome/scripts/wallpaper.fish`
-set -Ux CURRENT_WALLPAPER (random choice ~/Wallpapers/current/*)
-DISPLAY=:0 feh --bg-fill "$CURRENT_WALLPAPER"
+
+set LIKE_DIR ~/Pictures/Wallpapers
+set WALLPAPER_DIR ~/Wallpapers/current
+set CURRENT_WALLPAPER (realpath ~/Wallpapers/current/current)
+
+function dislike
+    if test -n "$CURRENT_WALLPAPER" -a -f "$CURRENT_WALLPAPER"
+        set dislike_dir (dirname "$CURRENT_WALLPAPER")/dislike/
+        [ -d "$dislike_dir" ] || mkdir -p "$dislike_dir"
+        mv $CURRENT_WALLPAPER "$dislike_dir"
+    end
+end
+
+function like
+    if test -n "$CURRENT_WALLPAPER" -a -f "$CURRENT_WALLPAPER"
+        cp $CURRENT_WALLPAPER "$LIKE_DIR" --backup=numbered
+    end
+end
+
+set -l options h/help n/not-change d/dislike l/like
+argparse $options -- $argv
+or return
+
+if set -q _flag_help
+    printf "Usage: wallpaper [OPTIONS]\n\n"
+    printf "Options:\n"
+    printf "  -h/--help         print this message\n"
+    printf "  -n/--not-change   update current but not set wallpaper\n"
+    printf "  -d/--dislike      dislike current wallpaper\n"
+    printf "  -l/--like         like current wallpaper\n"
+    return 0
+end
+
+if set -q _flag_dislike
+    dislike
+    return 0
+end
+
+if set -q _flag_like
+    like
+    return 0
+end
+
+ln -sf (random choice $WALLPAPER_DIR/*) $WALLPAPER_DIR/current
+if test -z $_flag_not-change
+    DISPLAY=:0 feh --bg-fill "$CURRENT_WALLPAPER"
+end
