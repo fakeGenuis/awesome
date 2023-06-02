802cd1a394989fc2b1bf4239a0233d25aacf0a6d
JiaRan <32349731+fakeGenuis@users.noreply.github.com>
2023-06-03 00:28:38 +0800

    [fix] redshift multi monitor and screen lock

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -9,7 +9,6 @@ function run {
 run xsettingsd -c ~/.cache/xsettingsd/xsettingsd.conf
 run /usr/bin/lxqt-policykit-agent
 run picom
-run redshift-gtk
 run kdeconnect-indicator
 run blueman-applet
 run qv2ray
@@ -18,6 +17,7 @@ run xcape -e "Control_L=Escape"
 
 fcitx5 -rd
 pidof nextcloud || nextcloud &
+pidof redshift || redshift &
 
 # systemd (=~/.config/systemd/user/emacs.service=) emacs cannot inherit
 # environment variables in shell startup
@@ -27,7 +27,7 @@ emacsclient -e "1" &>/dev/null || env LC_CTYPE='zh_CN.UTF-8' emacs --daemon &
 xset s 910
 
 # set current screen out
-_OUT=$(xrandr | grep " connected" | cut -d' ' -f1)
+_OUT=$(xrandr | grep " connected" | cut -d' ' -f1 | head -1)
 # Run xidlehook
 run xidlehook \
   --detect-sleep \
@@ -38,11 +38,11 @@ run xidlehook \
   `# Dim the screen after 60 seconds, undim if user becomes active` \
   --timer 900 \
   `# xrandr --output "$PRIMARY_DISPLAY" --brightness .1` \
-  'for i in $(seq 1 50); do sleep 0.02 && xrandr --output '"$_OUT"' --brightness $(echo "1 - 0.01*$i" | bc); done' \
-  'xrandr --output '"$_OUT"' --brightness 1' \
+  'pkill -9 redshift; for i in $(seq 1 50); do sleep 0.02 && xrandr --output '"$_OUT"' --brightness $(echo "1 - 0.01*$i" | bc); done' \
+  'xrandr --output '"$_OUT"' --brightness 1 & redshift' \
   `# Undim & lock after 10 more seconds` \
-  --timer 10 \
-  'xrandr --output '"$_OUT"' --brightness 1 && betterlockscreen -l' \
+  --timer 20 \
+  'xrandr --output '"$_OUT"' --brightness 1; redshift& betterlockscreen -l' \
   '' \
   `# Finally, suspend an hour after it locks`
 #   --timer 3600 \
