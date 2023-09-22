a320aef24d97a22f1ce8277b855924a3d82af27a
PRD <32349731+fakeGenuis@users.noreply.github.com>
2023-09-22 09:11:15 +0800

    [remove] emacs daemon at startup

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -20,7 +20,7 @@ pidof redshift || redshift &
 
 # systemd (=~/.config/systemd/user/emacs.service=) emacs cannot inherit
 # environment variables in shell startup
-emacsclient -e "1" &>/dev/null || env LC_CTYPE='zh_CN.UTF-8' emacs --daemon &
+# emacsclient -e "1" &>/dev/null || env LC_CTYPE='zh_CN.UTF-8' emacs --daemon &
 
 # set screen saver time
 xset s 920
